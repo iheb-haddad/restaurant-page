912b098053b9b150e2690d39a60de7c40ea785a8
Iheb haddad <100970145+iheb-haddad@users.noreply.github.com>
2024-05-01 01:42:02 +0100

    Update app.js

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ window.addEventListener('scroll',() =>{
         document.getElementById("active1").style.background="#ddb892";
         document.getElementById("active2").style.background="none";
        document.getElementById("active3").style.background="none";
-    }else if(scrolled<1440)
+    }else if(scrolled<1340)
     {
         document.getElementById("active2").style.background="#ddb892";
         document.getElementById("active1").style.background="none";
